82a080ed63e15cb7444bf4e52d08d8fffe8d9001
korrykatti <143781663+KorryKatti@users.noreply.github.com>
2026-03-19 19:25:38 +0530

    damn

---
+++ apps/ollam_ui/rend_backend/shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    (pkgs.python3.withPackages (ps: [
+      ps.fastapi
+      ps.uvicorn
+      ps.pydantic
+    ]))
+    pkgs.bash
+    pkgs.coreutils
+    pkgs.nix
+  ];
+}
